096e7d47a6d47a32e3b47bbcfbf67f4656ef172a
unknown <firgenscj@gmail.com>
2020-02-09 16:02:19 -0600

    Added jquery - added in ajax call

--- src/App.js
+++ src/App.js
@@ -2,19 +2,44 @@ import React, { Component } from 'react';
 import './App.scss';
 import Banner from './components/Banner';
 import 'font-awesome/css/font-awesome.min.css';
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Banner />
-      <div className="wrapper">
-        <div className="search_box">
-          <input type="text" placeholder="Search for a spell!" />
-          <i className="fa fa-search" />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.performSearch();
+  }
+
+  performSearch() {
+    const urlString = 'http://www.dnd5eapi.co/api/spells/?name=magic';
+    $.ajax({
+      url: urlString,
+      success: searchResults => {
+        const results = searchResults.results;
+        console.log(results);
+      },
+      error: (xhr, status, err) => {
+        console.error('Failed to fetch data');
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Banner />
+        <div className="wrapper">
+          <div className="search_box">
+            <input type="text" placeholder="Search for a spell!" />
+            <i className="fa fa-search" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
